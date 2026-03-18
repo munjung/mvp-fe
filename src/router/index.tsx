12245b55be684aa556bc5474b5ea04da84832c46
ahyeon <ahyeon.choe@kt.com>
2026-03-18 15:53:12 +0900

    Del: 테스트용 파일 제거 간단순차적진행..

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,14 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom'
 import RootLayout from '@/layouts/RootLayout'
-import PortalPage from '@/pages/PortalPage'
 import GuidePage from '@/pages/GuidePage'
 import CardList from '@/pages/CardList'
 import CardDetail from '@/pages/CardDetail'
-import ClaimsAgentMVP from '@/pages/mvps/ClaimsAgentMVP'
-import SecurityDashboard from '@/pages/mvps/SecurityDashboard'
-import AIFirewall from '@/pages/mvps/AIFirewall'
-import StockPilot from '@/pages/mvps/StockPilot'
-import ClaimsAgentNew from '@/pages/mvps/ClaimsAgentNew'
 
 export const router = createBrowserRouter([
   {
@@ -20,14 +14,6 @@ export const router = createBrowserRouter([
 
       /* 가이드 화면 */
       { path: 'guide', element: <GuidePage /> },
-      /* 테스트용 */
-      { path: 'mvps', element: <PortalPage /> },
-
-      { path: 'mvps/claims', element: <ClaimsAgentMVP /> },
-      { path: 'mvps/security', element: <SecurityDashboard /> },
-      { path: 'mvps/firewall', element: <AIFirewall /> },
-      { path: 'mvps/stockpilot', element: <StockPilot /> },
-      { path: 'mvps/claimsNew', element: <ClaimsAgentNew /> },
     ],
   },
 ])
