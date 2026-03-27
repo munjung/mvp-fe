fdeb7ad55a2cdb7f0cc2bd91f0a6b1f75d9fc0f3
ahyeon <ahyeon.choe@kt.com>
2026-03-27 16:17:29 +0900

    feat: 파손부위 api 추가

--- src/hooks/useEstimate.ts
+++ src/hooks/useEstimate.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import { getBrandList } from '@api/estimate'
+import { getBrandList, getDamageList } from '@api/estimate'
 
 export const useBrands = () => {
   return useQuery({
@@ -7,6 +7,17 @@ export const useBrands = () => {
     queryFn: async () => {
       const res = await getBrandList()
       console.log('제조사 목록 조회 :: ', res.data)
+      return res.data
+    },
+  })
+}
+
+export const useDamages = () => {
+  return useQuery({
+    queryKey: ['damages'],
+    queryFn: async () => {
+      const res = await getDamageList()
+      console.log('파손부위 목록 조회 :: ', res.data)
       return res.data
     },
   })
