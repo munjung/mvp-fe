81126934399e52e8e71c7968f006bab89b798345
ahyeon <ahyeon.choe@kt.com>
2026-04-01 08:59:19 +0900

    desing: 탭 정리

--- src/components/tabs/InjuryTab.tsx
+++ src/components/tabs/InjuryTab.tsx
@@ -29,12 +29,28 @@ function InjuryTab({ selectedValue, onSelectChange }: Props) {
         onViewSituation={() => {}}
       />
       {/* TODO: 대인피해 기능 구현 */}
-      <BaseSection className="mt-20" title="대인 접수 내용">
-        <BaseTextarea></BaseTextarea>
-      </BaseSection>
-      <BaseButton className="mt-10 w100" onClick={() => console.log('클릭')}>
-        대인 분석 실행
-      </BaseButton>
+      <div className="estimate-layout mt-20">
+        <div className="estimate-layout__left">
+          <BaseSection className="mt-20" title="대인 접수 내용">
+            <BaseTextarea placeholder="대인 접수 정보를 입력하세요... 예: 접수자 정보, 상해 내용, 진단서, 치료비, 위자료 등"></BaseTextarea>
+          </BaseSection>
+          <BaseButton className="mt-10 w100" onClick={() => console.log('실행')}>
+            대인 분석 실행
+          </BaseButton>
+        </div>
+
+        <div className="estimate-layout__right">
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '100px',
+            }}
+          >
+            대인 접수 내용 입력 후 분석을 실행하세요
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/tabs/ProcessTab.tsx
+++ src/components/tabs/ProcessTab.tsx
@@ -1,8 +1,9 @@
 // AI 자동차 손해사정 > 처리 방법 탭
+import { useState } from 'react'
 import type { Card } from '@api/cards'
 import TabHeader from './TabHeader'
 
-import { BaseSection, BaseButton, BaseTextarea } from '@components/common'
+import { BaseSection, BaseButton, BaseTextarea, BaseRadio } from '@components/common'
 
 interface Props {
   card: Card
@@ -10,7 +11,20 @@ interface Props {
   onSelectChange: (value: string) => void
 }
 
+const radioOptions = [
+  { label: '520d 사이드 미러+범퍼 파손', value: 'a' },
+  { label: 'GV80 전면 5부위 심각', value: 'b' },
+  { label: '아반떼 후미 충돌', value: 'c' },
+]
+
 function ProcessTab({ selectedValue, onSelectChange }: Props) {
+  const [radioVal, setRadioVal] = useState('')
+
+  // [FUNC] 라디오 버튼 변경 핸들러
+  const handleRadioChange = (value: string) => {
+    setRadioVal(value)
+  }
+
   return (
     <section>
       <TabHeader
@@ -33,6 +47,7 @@ function ProcessTab({ selectedValue, onSelectChange }: Props) {
       <BaseButton>사고건 불러오기</BaseButton>
       <BaseSection className="mt-20" title="사고건 내용 입력">
         <BaseTextarea></BaseTextarea>
+        <BaseRadio options={radioOptions} value={radioVal} onChange={handleRadioChange} />
       </BaseSection>
 
       <BaseButton className="mt-10 w100" onClick={() => console.log('클릭')}>
